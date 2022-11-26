e6cd4b17d3bc0d82c4da8917177208b30c6fdf79
SHUBH-17 <34864905+SHUBH-17@users.noreply.github.com>
2022-11-26 20:55:44 +0530

    Update A_Python_Program_1.py

--- A_Python_Program_1.py
+++ A_Python_Program_1.py
@@ -2,22 +2,23 @@ arr = ["Daisy","Rose","Hyacinth","Poppy"]
 myarr=[]
 maxLength = len(max(arr, key=len))
 
-for str in arr:
-    if len(str) < maxLength:
-        count = len(str)
+for str1 in arr:
+    if len(str1) < maxLength:
+        count = len(str1)
         while count < maxLength:
-            str += '*'
+            str1 += '*'
             count+=1
-        myarr.append(str)
+        myarr.append(str1)
     else:
-        myarr.append(str)
-#print(myarr)
-
+        myarr.append(str1)
+print(myarr)
+input = " ".join(map(str,myarr))
+print(input)
 
 charBuffer = []
-def processWords(input):
+def processWords(input,maxlen):
     s = input.split(" ")
-    for i in range(0,4):
+    for i in range(0,maxlen):
         for values in s:
             if values[i] == '0':
                 continue
@@ -25,6 +26,6 @@ def processWords(input):
                 charBuffer.append(values[i])
     return charBuffer
 
-input = " ".join(map(str,myarr))
-result = processWords(input)
+#input = " ".join(map(str,arr))
+result = processWords(input,maxLength)
 print(*result, sep = "")
